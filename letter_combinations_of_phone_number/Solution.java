9e0a53d269b6abcb3c1da2be0bbcc3e196daa4cd
SaadAfzal <saadafzal89@gmail.com>
2017-05-09 23:07:18 -0400

    First Commit

---
+++ letter_combinations_of_phone_number/Solution.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.HashMap;
+public class Solution{
+
+    HashMap<Integer, ArryayList<String>> hm = new HashMap<>();
+    public void initHashMap(){
+
+    }
+    public List<String> letterCombinations(String digits) {
+
+    }
+    public static void main(String args[]){
+        String input = "23";
+        Solution obj = new Solution();
+
+    }
+}
